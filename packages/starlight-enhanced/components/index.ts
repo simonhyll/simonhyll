578d92cd3dc364f9ee111c841efe0f1b398cb574
Simon Hyll <hyllsimon@gmail.com>
2024-11-26 16:26:02 +0100

    feat: working on features

--- packages/starlight-enhanced/components/index.ts
+++ packages/starlight-enhanced/components/index.ts
@@ -3,5 +3,7 @@ import SiteTitle from './SiteTitle.astro';
 import Header from './Header.astro';
 import Sidebar from './Sidebar.astro';
 import PageFrame from './PageFrame.astro';
+import Feature from './Feature.astro';
+import Features from './Features.astro';
 
-export { ThemeSelect, SiteTitle, Header, Sidebar, PageFrame };
+export { ThemeSelect, SiteTitle, Header, Sidebar, PageFrame, Feature, Features };
